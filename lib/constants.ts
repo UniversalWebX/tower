b876b54d6d2629d20813958a4f011857ccd2a2f6
WebX <darianbayan2@gmail.com>
2026-04-16 16:36:08 -0700

    Refactor database management scripts in package.json and enhance .gitignore to better exclude local development files.

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const TAG_MIN = 7;
+export const TAG_MAX = 30;
+export const TOPIC_COUNT = 5;
+export const SESSION_COOKIE = "tower_session";
+export const SESSION_MS = 1000 * 60 * 60 * 24 * 30;
+export const FEED_CANDIDATE_POOL = 320;
+export const MESSAGE_PAGE = 50;
